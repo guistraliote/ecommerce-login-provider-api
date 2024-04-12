50c2490830cea3b07b83d76b7f77dca254477314
Guilherme Straliote <gui.ssh@gmail.com>
2024-04-12 20:32:43 -0300

    first commit

---
+++ src/main/java/com/guistraliote/LoginProviderApiApplication.java
@@ -0,0 +1,13 @@
+package com.guistraliote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginProviderApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginProviderApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guistraliote/LoginProviderApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guistraliote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginProviderApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
